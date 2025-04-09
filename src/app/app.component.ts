36cd0aa66b649088e1411deaa1636aa4ca86ae73
mrtksknn <mertkeskinmtk6@gmail.com>
2025-04-09 23:47:39 +0300

    Create responsive and functional rating card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'rating-card';
+  submited = false;
+  count = Array(5).fill(0);
+  selectedValue: number | null = null;
+
+  selectRating(index: number): void {
+    this.selectedValue = index + 1;
+  }
+
+  submitRate() {
+    if (this.selectedValue) {
+      this.submited = true;
+    }
+    return
+  }
 }
